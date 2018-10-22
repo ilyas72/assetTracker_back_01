9aeff6b537bdaae9729fd233e05f2fb8735c9549
ilyas72 <muhammadilyasjamil@gmail.com>
2018-10-22 17:21:19 +0800

    new

--- dbconfig.js
+++ dbconfig.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	host: 'localhost', port: 3306,
-	user: 'root', password: 'khalifah',
+	user: 'root', password: 'khalifaH@1234',
 	database: 'AssetTracker_LoanSystem',
 	connectionLimit: 4
 }
